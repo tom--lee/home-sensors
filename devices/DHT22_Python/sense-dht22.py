b14b96e43f96c1f2db17debe2a7d4a3103ce9670
Tom Lee <tomlee@eelmot.com>
2023-01-02 21:52:01 +1100

    Modify and rename sensing script

---
+++ devices/DHT22_Python/sense-dht22.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import dht22
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+
+# read data using pin 14
+instance = dht22.DHT22(pin=4)
+
+time.sleep(1)
+
+result = instance.read()
+while (not result.is_valid()):
+    result = instance.read()
+time = datetime.datetime.utcnow().isoformat(timespec = "milliseconds") + 'Z'
+temperature = "%-3.1f" % result.temperature
+humidity = "%-3.1f" % result.humidity
+print("{} {} {}".format(time, temperature, humidity))
+
+GPIO.cleanup()
